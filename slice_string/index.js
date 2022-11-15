2d01e773e2ed0c963a6f966194e756593340acae
myang5t3r <matthew.yang@hunterdouglas.com>
2022-11-14 20:42:16 -0700

    more string manupulation with slice

--- slice_string/index.js
+++ slice_string/index.js
@@ -9,3 +9,9 @@ const newStr = str.slice(5);
 console.log(
   `To remove beginning of string use slice(x), where x is the number of characters you want to remove, for example str.slice(5) = ${newStr}`
 );
+
+// remove my name is from the string
+const nextStr = str.slice(0, 5) + str.slice(16);
+console.log(
+  `To remove my name is from str use str.slice(0, 5) + str.slice(16), which will log  = ${nextStr}`
+);
