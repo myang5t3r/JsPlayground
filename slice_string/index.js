a0d18f6fddc7d000c7cc0bf9719ce1689fdffdc8
myang5t3r <matthew.yang@hunterdouglas.com>
2022-11-14 20:50:56 -0700

    More string manupluation with slice, negative indexes

--- slice_string/index.js
+++ slice_string/index.js
@@ -15,3 +15,9 @@ const nextStr = str.slice(0, 5) + str.slice(16);
 console.log(
   `To remove my name is from str use str.slice(0, 5) + str.slice(16), which will log  = ${nextStr}`
 );
+
+// Practice negative indexes print my name is
+const negStr = str.slice(6, -5);
+console.log(
+  `We can also use negative indexes to remove backend of string, us str.slice(6,-5) to remove first 6 indices and last 5 indices  = ${negStr}`
+);
